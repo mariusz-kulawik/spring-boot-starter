e74df204a44c0d26175235482141abdd0e98f820
Mariusz Kulawik <mkulawik@sygnity.pl>
2016-10-25 15:38:22 +0200

    prime faces

---
+++ src/main/java/pl/mkulawik/main/Main.java
@@ -0,0 +1,39 @@
+package pl.mkulawik.main;
+
+import javax.faces.webapp.FacesServlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {""})
+@EnableAutoConfiguration
+public class Main extends SpringBootServletInitializer
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(Main.class,args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+  {
+    return application.sources(Main.class);
+  }
+
+  @Bean
+  public ServletRegistrationBean servletRegistrationBean()
+  {
+    FacesServlet servlet = new FacesServlet();
+    ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet,"*.jsf");
+    return servletRegistrationBean;
+  }
+
+}
